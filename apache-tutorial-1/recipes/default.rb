f874d2eafd2a2f4ec375e427361d45b66268fd47
vijay32 <vijayabhaskar@sparksupport.com>
2014-06-19 13:03:40 +0530

    Update to install mysql

--- apache-tutorial-1/recipes/default.rb
+++ apache-tutorial-1/recipes/default.rb
@@ -7,15 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#package 'apache2' do
-#  action :install
-#end
-
-#service 'apache2' do
-#  action [ :enable, :start ]
-#end
-
-cookbook_file '/var/www/index.html' do
-  source 'index.html'
-  mode '0644'
+package 'mysql' do
+  action :install
 end
+
+service 'mysql' do
+  action [ :enable, :start ]
+end
+
+#cookbook_file '/var/www/index.html' do
+#  source 'index.html'
+#  mode '0644'
+#end
